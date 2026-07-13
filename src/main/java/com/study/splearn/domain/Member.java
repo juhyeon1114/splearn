4cd78fe54ee9319de7cc3edcccaca80465aa9878
김주현 <juhyeon1114@gmail.com>
2026-07-13 21:57:32 +0900

    Add Member status management and validation tests
    멤버 상태 관리 메서드(activate, deactivate)를 추가하고, 상태 변경 시의 유효성 검증 로직을 구현했습니다. 또한, 각 상태 전환 시나리오에 대한 테스트 코드를 작성해 로직의 안정성을 확인했습니다.

--- src/main/java/com/study/splearn/domain/Member.java
+++ src/main/java/com/study/splearn/domain/Member.java
@@ -1,20 +1,38 @@
 package com.study.splearn.domain;
 
+import static java.util.Objects.*;
+import static org.springframework.util.Assert.*;
+
 import lombok.Getter;
 
 @Getter
 public class Member {
 
-	private final String email;
-	private final String nickname;
-	private final String passwordHash;
-	private final MemberStatus status;
+	private String email;
+
+	private String nickname;
+
+	private String passwordHash;
+
+	private MemberStatus status;
 
 	public Member(String email, String nickname, String passwordHash) {
-		this.email = email;
-		this.nickname = nickname;
-		this.passwordHash = passwordHash;
+		this.email = requireNonNull(email);
+		this.nickname = requireNonNull(nickname);
+		this.passwordHash = requireNonNull(passwordHash);
 		this.status = MemberStatus.PENDING;
+	}
+
+	public void activate() {
+		state(this.status == MemberStatus.PENDING, "PENDING 상태가 아닙니다.");
+
+		this.status = MemberStatus.ACTIVE;
+	}
+
+	public void deactivate() {
+		state(this.status == MemberStatus.ACTIVE, "ACTIVE 상태가 아닙니다.");
+
+		this.status = MemberStatus.DEACTIVATED;
 	}
 
 }
--- src/test/java/com/study/splearn/domain/MemberTest.java
+++ src/test/java/com/study/splearn/domain/MemberTest.java
@@ -19,4 +19,90 @@ class MemberTest {
 		assertThat(member.getStatus()).isEqualTo(MemberStatus.PENDING);
 	}
 
+	@Test
+	@DisplayName("NPE 테스트")
+	void test2() {
+		assertThatThrownBy(() -> new Member(
+			null,
+			"nickname",
+			"password"
+		)).isInstanceOf(NullPointerException.class);
+
+		assertThatThrownBy(() -> new Member(
+			"test@test.com",
+			null,
+			"password"
+		)).isInstanceOf(NullPointerException.class);
+
+		assertThatThrownBy(() -> new Member(
+			"test@test.com",
+			"nickname",
+			null
+		)).isInstanceOf(NullPointerException.class);
+	}
+
+	@Test
+	@DisplayName("멤버 가입 완료")
+	void test3() {
+		var member = new Member(
+			"test@test.com",
+			"nickname",
+			"password"
+		);
+
+		member.activate();
+
+		assertThat(member.getStatus())
+			.isEqualTo(MemberStatus.ACTIVE);
+	}
+
+	@Test
+	@DisplayName("멤버 가입 완료 실패")
+	void test12() {
+		var member = new Member(
+			"test@test.com",
+			"nickname",
+			"password"
+		);
+		member.activate();
+
+		assertThatThrownBy(member::activate)
+			.isInstanceOf(IllegalStateException.class);
+	}
+
+	@Test
+	@DisplayName("멤버 탈퇴")
+	void test132() {
+		var member = new Member(
+			"test@test.com",
+			"nickname",
+			"password"
+		);
+		member.activate();
+
+		member.deactivate();
+
+		assertThat(member.getStatus())
+			.isEqualTo(MemberStatus.DEACTIVATED);
+	}
+
+	@Test
+	@DisplayName("멤버 탈퇴 실패")
+	void test123() {
+		var member = new Member(
+			"test@test.com",
+			"nickname",
+			"password"
+		);
+
+		assertThatThrownBy(member::deactivate)
+			.isInstanceOf(IllegalStateException.class);
+
+		member.activate();
+		member.deactivate();
+
+		assertThatThrownBy(member::deactivate)
+			.isInstanceOf(IllegalStateException.class);
+	}
+
 }
